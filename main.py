159c29542a7c2af7a560465ec4b01aa0952ef60e
dogyun <kdk124578@naver.com>
2021-07-28 17:44:35 +0900

    도로명 + 구주소 합친 주소 처리

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ date = 1
 time = 2
 addr = 3
 
+def split_data_into_date(split_year):
+    ### 초기 데이터셋 (illegal_park_info.csv) 년도별로 파일 나눠서 저장.
+
+    with open('./illegal_park_info.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+
+        csv_file = open(split_year+'.csv', 'w', newline='', encoding='utf-8-sig')
+        writer = csv.writer(csv_file)
+
+        for row in reader:
+            # print(row)
+            year = row[1][:4]
+            if year == split_year:
+                writer.writerow(row)
+
 def get_geocode(address):
     ### 네이버 API로 주소 -> 좌표 리턴
 
@@ -37,7 +52,7 @@ def remove_overlap(file_name):
     ### 데이터셋에 중복되는 주소들 제거 후 주소만 txt로 저장
     addresses = {}
 
-    with open(file_name+'.csv', encoding='UTF8') as cf:
+    with open(file_name+'.csv', 'r', encoding='UTF8') as cf:
         
         ill_park = csv.reader(cf)
 
@@ -51,7 +66,7 @@ def remove_overlap(file_name):
     with open(file_name+'.txt', 'w', encoding='UTF-8') as f:
 
         for key in addresses.keys():
-
+            
             key = normalization(key)
 
             f.write(key + '\n')
@@ -59,8 +74,7 @@ def remove_overlap(file_name):
         f.close
 
 def normalization(address):
-    ### 검색 안되는 주소 전처리
-
+    ### 검색 안되는 주소 전처리 ###
 
     # 앞뒤 공백 지우기
     address = address.strip()
@@ -69,12 +83,14 @@ def normalization(address):
     if address[:2] == "북구":
         address = address[3:]
     
+    address = "대구 북구 " + address
+
     # 끝 단어 이상한거 제거
     if address[-1] == "부" or address[-1] == "소" or address[-1] == "장":
         address = address[:-1]
 
     # 검색에 방해되는 단어 지우기
-    ban_words = ["부근", "건너", "횡단보도", "횡단번호", "모퉁이", "소화전", "육교밑", "버스정류", "버스정류장", "어린이보호구역", "인도"]
+    ban_words = ["부근", "건너", "사거리", "횡단보도", "횡단번호", "모퉁이", "소화전", "육교밑", "버스정류", "버스정류장", "어린이보호구역", "인도"]
 
     for word in ban_words:
         address = re.sub(word, '', address)
@@ -83,22 +99,27 @@ def normalization(address):
     address = re.sub(r'\([^)]*\)', '', address)
     
     address = address.strip()
-    return "대구 북구 " + address
 
-def split_data_into_date(split_year):
-    ### 초기 데이터셋 (illegal_park_info.csv) 년도별로 파일 나눠서 저장.
+    address = gil_to_doro(address)
 
-    with open('./illegal_park_info.csv', 'r', encoding='utf-8') as f:
-        reader = csv.reader(f)
+    return address
 
-        csv_file = open(split_year+'.csv', 'w', newline='', encoding='utf-8-sig')
-        writer = csv.writer(csv_file)
+# normalization("복현동 대학로 142")
+def gil_to_doro(addr):
+    ### 동과 도로명 주소가 같이 있다면 제거함
+    
+    temp = addr.split(' ')
+    
+    if len(temp) < 4:
+        return addr
 
-        for row in reader:
-            # print(row)
-            year = row[1][:4]
-            if year == split_year:
-                writer.writerow(row)
+    if temp[2][-1] == "동" or temp[2][-1] == "가":
+        if temp[3][-1] == "로" or temp[3][-1] == "길":
+            del temp[3]
+
+    print(temp)
+    return " ".join(temp)
+
 
 def refactoring_file(file_name):
     ### 데이터셋 전처리 작업 후 저장
@@ -120,12 +141,10 @@ def refactoring_file(file_name):
 def check_ok(file_name):
     ### 검색 안 되는 주소 리스트 뽑기 ###
 
-    # f = open(file_name+'.csv', 'r', encoding='utf-8-sig')
     f = open(file_name+'.txt', 'r', encoding='utf-8-sig')
     result = open(file_name+'_checked.csv', 'w', newline='', encoding='utf-8-sig')
 
     writer = csv.writer(result)
-    # reader = csv.reader(f)
 
     ok_lists = []
     no_lists = []
@@ -147,11 +166,14 @@ def check_ok(file_name):
         print(cnt, end='\r')
         cnt += 1
 
-    writer.writerows(no_lists + ok_lists)
+    writer.writerows(no_lists)
     
 
 
 if "__main__":
 
-    # remove_overlap('2021')
+    remove_overlap('2021')
+    # remove_overlap('illegal_park_info')
     check_ok('2021')
+
+    pass
